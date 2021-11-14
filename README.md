edefabb24a0d47f189b15551c0246a33d879af03
Alaa Haamdi <alaahaamdi@spectrumgroupe.fr>
2021-11-14 15:17:10 +0000

    First Try Forge, insert macro Hello world with counting number of comments in the same inserted page macro

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# ForgeAtlassian
-Steps to build a cloud plugin
+# Developing with the Forge platform for Atlassian’s cloud products
+
+Steps:
+
+    ► Before you begin:
+    Node.js and Docker
+
+    ► Install the Forge CLI:
+    Install the Forge CLI globally by running:
+    npm install -g @forge/cli
+
+    Verify that the CLI is installed correctly by running:
+    forge --version
+
+    ► Log in with an Atlassian API token:
+    Go to https://id.atlassian.com/manage/api-tokens
+
+    ► Forge login:
+    forge login
+
+    ► Set up an Atlassian cloud developer site:
+    Go to http://go.atlassian.com/cloud-dev 
+
+    ► Create your app:
+    forge create
+    
+
+
+
+
+
---
+++ forge-learn/src/index.jsx
@@ -0,0 +1,30 @@
+import api, { route } from "@forge/api";
+import ForgeUI, { render, Fragment, Text, Macro, useProductContext, useState } from '@forge/ui';
+
+const fetchCommentsForContent = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(route`/wiki/rest/api/content/${contentId}/child/comment`);
+
+  const data = await res.json();
+  return data.results;
+};
+
+const App = () => {
+  const context = useProductContext();
+  const [comments] = useState(async () => await fetchCommentsForContent(context.contentId));
+  console.log("TEST");
+  console.log(`Number of comments on this page: ${comments.length}`);
+
+  return (
+    <Fragment>
+      <Text>Hello world!</Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <Macro
+    app={<App />}
+  />
+);
